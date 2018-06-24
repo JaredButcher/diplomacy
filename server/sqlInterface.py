71e13ad660a5219da99cde90eb7f3b55af2b24d0
Jared Butcher <jared.butcher1219@gmail.com>
2018-06-23 20:54:09 -0400

    Began sql class

--- server/sqlInterface.py
+++ server/sqlInterface.py
@@ -1,14 +1,33 @@
 import mysql.connector
+import secrets
+import hashlib
 
-dbConn = mysql.connector.connect(user='', password='', host='192.168.1.2', database='')
-cursor = dbConn.cursor()
+def connect(funct):
+    def decorator(self, *args, **kwargs):
+        self.conn = mysql.connector.connect(user=self.user, password=self.password, host=self.host, database=self.database)
+        self.cursor = self.conn.cursor()
+        funct(self, *args, **kwargs)
+        self.cursor.close()
+        self.conn.close()
+    return decorator
 
-query = 'SELECT username, name FROM users WHERE username=%s'
+class databaseConn:
+    def __init__(self, user, password, host, database):
+        self.user = user
+        self.password = password
+        self.host = host
+        self.database = database
 
-cursor.execute(query, ('bob',))
+    @connect
+    def addUser(self, username, password, name=None, email=None, phone=None):
+        query='INSERT INTO user (username, hash, salt, name, email, phone) VALUES (%s, %s, %s, %s, %s, %s);'
+        salt = secrets.randbits(128)
+        hash = hashlib.sha512(bytes(password, 'utf-8') + salt.to_bytes(16, byteorder='big')).hexdigest()
+        self.cursor.execute(query, username, hash, salt, name, email, phone)
 
-for value in cursor:
-    print(value)
-
-cursor.close()
-dbConn.close()
+    @connect
+    def test(self, name):
+         query='SELECT id, username, name FROM users WHERE name LIKE CONCAT(\'%\', %s, \'%\');'
+         self.cursor.execute(query, (name,))
+         for value in self.cursor:
+             print(value)
